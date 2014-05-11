cf8f4759256d91adbaeccbd650c875da9ab7e2d8
Bernie Chiu <bernie_chiu@hotmail.com>
2014-05-11 11:04:00 +0800

    Understand more object ancestors call

--- project_3.1.rb
+++ project_3.1.rb
@@ -1,3 +1,5 @@
-5.class.superclass.superclass.superclass.superclass
+# The fifth one returns nil, so its ancestors should be 4
+# 5.class.superclass.superclass.superclass.superclass
 
-# The fifth one returns nil, so its ancestors should be 4.
+p 5.class.ancestors.count
+# Another method for calling ancestors (this one includes module count)
--- project_3.2.rb
+++ project_3.2.rb
@@ -1,12 +1,18 @@
+# def print_class_and_superclasses(object)
+#   klass = object.class
+#   superklass = klass.superclass
+
+#   puts klass
+
+#   while superklass
+#     puts superklass
+#     superklass = superklass.superclass
+#   end
+# end
+
 def print_class_and_superclasses(object)
-  klass = object.class
-  superklass = klass.superclass
-
-  puts klass
-
-  while superklass
-    puts superklass
-    superklass = superklass.superclass
+  object.class.ancestors.each do |klass|
+    puts klass
   end
 end
 
